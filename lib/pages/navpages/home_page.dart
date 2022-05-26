3e6eda46213ee0a108bdd3631fbb4ad29fa41f3c
shagun <shagun2001ch@gmail.com>
2022-05-26 14:18:27 +0530

    completed till 1:36:34

--- lib/pages/navpages/home_page.dart
+++ lib/pages/navpages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:traviaa/misc/colors.dart';
 import 'package:traviaa/widgets/app_large_text.dart';
+
 
 class HomePage extends StatefulWidget {
   const HomePage({ Key? key }) : super(key: key);
@@ -8,9 +10,10 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
+    TabController _tabController = TabController(length: 3, vsync: this);
     return Scaffold(
       //main column starts from here
       body: Column(crossAxisAlignment: CrossAxisAlignment.start,
@@ -34,11 +37,95 @@ class _HomePageState extends State<HomePage> {
           ]),
         ),
         SizedBox(height: 40,),
+        //discover text container
         Container(
           margin: EdgeInsets.only(left: 20),
-          child: AppLargeText(text: "DISCOVER"),
-        )
+          child: AppLargeText(text: "Discover"),
+        
+        ),
+         SizedBox(height: 30,),
+         Container(
+           
+           child: Align(
+             alignment: Alignment.centerLeft,
+             child: TabBar(
+               indicator: circleTabIndicator(color: AppColors.mainColor, radius: 4),
+               indicatorSize: TabBarIndicatorSize.label,
+               labelPadding: const EdgeInsets.only(left: 20,right: 20),
+               isScrollable: true,
+               labelColor: Colors.black,
+               unselectedLabelColor: Colors.grey,
+               controller: _tabController
+               ,tabs: [
+               
+               Tab(text: "Places"),
+               Tab(text:"Inspirations"),
+               Tab(text: "Emotions",)
+             ]),
+           ),
+         ),
+         Container(
+           padding: const EdgeInsets.only(left: 20),
+           height: 300,
+           width: double.maxFinite,
+           child: TabBarView(
+             controller: _tabController,
+             children: [
+               ListView.builder(
+                 scrollDirection: Axis.horizontal,
+                 itemCount: 3,
+                 itemBuilder: (BuildContext context, int index) { 
+                   return Container(
+                     margin: const EdgeInsets.only(right: 15,top: 10),
+                   width: 200,
+                   height: 300,
+                   decoration: BoxDecoration(
+                     borderRadius: BorderRadius.circular(20),
+                     image: DecorationImage(image: AssetImage("lib/img/mountain.jpeg"),
+                     fit: BoxFit.cover)
+                     
+                   ),
+                 );
+                  },
+                  
+               ),
+              Text("there"),
+              Text("bye")
+             ],
+           ),
+         ),
+      //start from here now
+      
       ]),
     );
   }
 }
+
+class circleTabIndicator extends Decoration{
+  final Color color;
+  double radius;
+  circleTabIndicator({required this.color,required this.radius});
+  @override
+  BoxPainter createBoxPainter([VoidCallback? onChanged]) {
+    // TODO: implement createBoxPainter
+    return _circlePainter(color:color,radius:radius);
+  }
+
+ 
+
+}
+ class _circlePainter extends BoxPainter{
+   final Color color;
+  double radius;
+  _circlePainter({required this.color,required this.radius});
+  @override
+  void paint(Canvas canvas, Offset offset, ImageConfiguration configuration) {
+    // TODO: implement paint
+    Paint _paint=Paint();
+    _paint.color=color;
+    _paint.isAntiAlias=true;
+    final Offset circleOffset =Offset(configuration.size!.width/2-radius/2,configuration.size!.height);
+    canvas.drawCircle(offset+circleOffset, radius, _paint);
+  }
+    
+  } 
